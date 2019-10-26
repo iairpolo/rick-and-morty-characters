ca7b78236329373ae5adae60cc146fd47693248a
Iair Poloniecki <iairpoloniecki@tuta.io>
2019-10-26 01:09:02 -0300

    Diseñados los items de la lista

--- src/components/CharacterItem.jsx
+++ src/components/CharacterItem.jsx
@@ -4,6 +4,9 @@ import './styles/CharacterItem.css';
 const CharacterItem = ({ character }) => (
   <li className='CharacterItem'>
     <img src={character.image} alt='' />
+    <div className='CharacterItem__Back'>
+      <h4 className='CharacterItem__Name'>{character.name}</h4>
+    </div>
   </li>
 );
 
